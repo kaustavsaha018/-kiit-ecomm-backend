dd7402a07c267aa1b2197c09765f873206ca75d5
Kaustav Saha <64833209+kaustavsaha018@users.noreply.github.com>
2022-11-27 00:02:32 +0530

    payment issue resolved

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -7,8 +7,8 @@ exports.processPayment = catchAsyncErrors(async (req, res, next) => {
 
     const paymentIntent = await stripe.paymentIntents.create({
         amount: req.body.amount,
-        currency: 'usd',
-
+        currency: 'inr',
+        
         metadata: { integration_check: 'accept_a_payment' }
     });
 
